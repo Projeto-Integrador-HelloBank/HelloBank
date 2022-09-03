52f001076b7ef2479e03ffed56345be1d294bf25
gerson espejo <gersonespejo127@gmail.com>
2022-09-03 18:54:53 -0300

    first commit

---
+++ src/main/java/com/br/hellorbank/HellorbankApplication.java
@@ -0,0 +1,13 @@
+package com.br.hellorbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellorbankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HellorbankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/hellorbank/HellorbankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.hellorbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HellorbankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
