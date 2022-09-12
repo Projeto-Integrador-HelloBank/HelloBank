2d9e4faac66d99f9647e2c0ae48dd0249a03fc28
alexandreps1123 <alexandre.ps1123@gmail.com>
2022-09-12 17:49:50 -0300

    atualização de testes

--- src/test/java/br/com/hellobank/repository/ClienteRepositoryTest.java
+++ src/test/java/br/com/hellobank/repository/ClienteRepositoryTest.java
@@ -1,6 +1,6 @@
 package br.com.hellobank.repository;
 
-
+import br.com.hellobank.model.Cliente;
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
@@ -8,32 +8,42 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import br.com.hellobank.model.Cliente;
-
 @RunWith(SpringRunner.class)
 @DataJpaTest
 class ClienteRepositoryTest {
 
-	@Autowired
-	private ClienteRepository repository;
-	
-	
-	@Test
- 	public void clienteCPF() {
-		
+  @Autowired
+  private ClienteRepository sobreTeste;
 
- 		String clienteCPF = "13254678915";
- 		Cliente cliente = repository.findByCpf(clienteCPF);
- 		Assert.assertNotNull(cliente);
- 		Assert.assertEquals (clienteCPF, cliente.getCpf());
- 	}
+  @Test
+  public void clienteCPF() {
+
+    String clienteCpf = "796.881.490-12";
+
+    Cliente clienteA = new Cliente();
+		clienteA.setNome("Spring");
+		clienteA.setCpf(clienteCpf);
+		clienteA.setEndereco("rua 20 oloko meu ");
+		clienteA.setCidade("catalao");
+		clienteA.setEstado("GO");
+		clienteA.setEmail("spring@tudodebom.com");
+		clienteA.setTelefone("+55(00)11111-1111");
 	
-	@Test
- 	public void clienteCPFErrado() {
- 		String clienteCPF = "23.555.789-00";
- 		Cliente cliente = repository.findByCpf(clienteCPF);
- 		Assert.assertNull(cliente);
- 	}
-	}
+    sobreTeste.save(clienteA);
+
+    Cliente cliente = sobreTeste.findByCpf(clienteCpf);
+    Assert.assertNotNull(cliente);
+    Assert.assertEquals(clienteCpf, cliente.getCpf());
+
+  }
 
 
+  @Test
+  public void clienteCPFErrado() {
+
+    String clienteCPF = "23.555.789-00";
+    
+    Cliente cliente = sobreTeste.findByCpf(clienteCPF);
+    Assert.assertNull(cliente);
+  }
+}
--- src/test/java/br/com/hellobank/repository/ContaRepositoryTest.java
+++ src/test/java/br/com/hellobank/repository/ContaRepositoryTest.java
@@ -1,29 +1,44 @@
 package br.com.hellobank.repository;
 
-import static org.junit.jupiter.api.Assertions.*;
+// import static org.junit.Assert.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
+import java.math.BigDecimal;
 
-import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import br.com.hellobank.model.Cliente;
+import br.com.hellobank.model.Conta;
 
 @RunWith(SpringRunner.class)
 @DataJpaTest
-
 class ContaRepositoryTest {
 
 	
-		@Autowired
-		private ContaRepository repository;
-		@Test
-	 	public void clienteCPF() {
-	 		String clienteCPF = "23.456.789-00";
-	 		Cliente cliente = repository.findByCpf(clienteCPF);
-	 		Assert.assertNotNull(cliente);
-	 		Assert.assertEquals (clienteCPF, cliente.getCpf());
-	 	}
+	@Autowired
+    private ContaRepository sobreTeste;
+
+    @Test
+    void deveriaVerificarSeContaExitePeloNumeroConta() {
+        
+        // dado
+        String numeroConta = "123456789";
+        Conta conta = new Conta();
+
+        conta.setTipoConta("Poupança");
+        conta.setNumeroConta(numeroConta);
+        conta.setAgencia("4567");
+        conta.setSaldo(new BigDecimal(1500.00));
+
+        sobreTeste.save(conta);
+
+        // quando
+        Conta esperado = sobreTeste.findByNumeroConta(numeroConta);
+
+        // entao
+        assertThat(numeroConta).isEqualTo(esperado.getNumeroConta());
+    }
+	
 }
